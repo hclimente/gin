80d7d5aaa5447d112c40184383754e88647ee5b4
Hector Climente <hclimente@gmail.com>
2017-02-24 14:50:34 +0100

    finished

--- tests/scones2_tests/SCones2Test.cc
+++ tests/scones2_tests/SCones2Test.cc
@@ -10,6 +10,12 @@ struct CSconesInitialSettings{
     float64 expected_eta;
     float64 lambda;
     float64 expected_lambda;
+    uint test_statistic;
+    float64 expected_association;
+    float64 expected_connectivity;
+    float64 expected_sparsity;
+    string path_prefix;
+    int expected_causal_SNPs[10];
 
 };
 
@@ -19,14 +25,15 @@ struct SearchMarkers : public ::testing::Test, testing::WithParamInterface<CScon
     CScones* scones;
     float64 eta;
     float64 lambda;
+    GWASData tmpData;
 
     SearchMarkers(){
 
         GWASData data;
 
-        string genotype_str = "data/testing/scones/genotype";
-        string phenotype_str = "data/testing/scones/phenotype.txt";
-        string network_str = "data/testing/scones/network.txt";
+        string genotype_str = GetParam().path_prefix + "genotype";
+        string phenotype_str = GetParam().path_prefix + "phenotype.txt";
+        string network_str = GetParam().path_prefix + "network.txt";
         uint encoding = 0;
         float64 maf = 0.05;
 
@@ -36,19 +43,17 @@ struct SearchMarkers : public ::testing::Test, testing::WithParamInterface<CScon
         CGWASDataHelper::encodeHeterozygousData(&data,encoding);
         CGWASDataHelper::filterSNPsByMAF(&data,maf);
         CSconesIO::readSparseNetworkFile(network_str,&data);
-        GWASData tmpData = CGWASDataHelper::removeSamples4MissingData(data,0);
+        tmpData = CGWASDataHelper::removeSamples4MissingData(data,0);
 
-        settings = CSconesSettings();
         eta = GetParam().eta;
         lambda = GetParam().lambda;
-        cout << eta << "\t" << GetParam().expected_eta << "\n";
-        cout << lambda << "\t" << GetParam().expected_lambda << "\n";
 
+        settings = CSconesSettings();
         settings.folds = 10;
         settings.seed = 0;
         settings.selection_criterion = CONSISTENCY;
         settings.selection_ratio = 0.8;
-        settings.test_statistic = SKAT;
+        settings.test_statistic = GetParam().test_statistic;
         settings.nParameters = 10;
         settings.evaluateObjective = false;
         settings.dump_intermediate_results = true;
@@ -78,69 +83,8 @@ struct SearchMarkers : public ::testing::Test, testing::WithParamInterface<CScon
     }
 };
 
-struct CSconesTest : public ::testing::Test {
-    CSconesSettings settings;
-    CScones* scones;
-
-    CSconesTest()
-    {
-        GWASData data;
-
-        string genotype_str = "data/testing/scones/genotype";
-        string phenotype_str = "data/testing/scones/phenotype.txt";
-        string network_str = "data/testing/scones/network.txt";
-        uint encoding = 0;
-        float64 maf = 0.05;
-
-        CPlinkParser::readPEDFile(genotype_str + ".ped", &data);
-        CPlinkParser::readMAPFile(genotype_str + ".map", &data);
-        CPlinkParser::readPhenotypeFile(phenotype_str,&data);
-        CGWASDataHelper::encodeHeterozygousData(&data,encoding);
-        CGWASDataHelper::filterSNPsByMAF(&data,maf);
-        CSconesIO::readSparseNetworkFile(network_str,&data);
-        GWASData tmpData = CGWASDataHelper::removeSamples4MissingData(data,0);
-
-        settings = CSconesSettings();
-        settings.folds = 10;
-        settings.seed = 0;
-        settings.selection_criterion = CONSISTENCY;
-        settings.selection_ratio = 0.8;
-        settings.test_statistic = SKAT;
-        settings.autoParameters = true;
-        settings.nParameters = 10;
-        settings.lambdas = VectorXd::Zero(settings.nParameters);
-        settings.etas = VectorXd::Zero(settings.nParameters);
-        settings.evaluateObjective = false;
-        settings.dump_intermediate_results = true;
-        settings.dump_path = "tmp/";
-
-        scones = new CScones(tmpData.Y.col(0),tmpData.X,tmpData.network, settings);
-    }
-
-    ~CSconesTest()
-    {
-        delete scones;
-    }
-};
-
-TEST_F(CSconesTest, integrity_selectedSNPs) {
-    scones -> test_associations();
-    int out = scones -> getIndicatorVector().sum();
-    EXPECT_EQ(10, out);
-    EXPECT_EQ(1, scones -> getIndicatorVector()(676));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(679));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(680));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(682));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(684));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(685));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(686));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(690));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(695));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(696));
-
-}
-
-TEST_F(CSconesTest, integrity_objectiveFunctionTerms) {
+TEST_P(SearchMarkers, checkObjectiveFunctionTerms) {
+    auto as = GetParam();
     scones -> test_associations();
     float64 lambda = scones -> getBestLambda();
     float64 eta = scones -> getBestEta();
@@ -150,13 +94,13 @@ TEST_F(CSconesTest, integrity_objectiveFunctionTerms) {
     double connectivity = terms(1);
     double sparsity = terms(2);
 
-    EXPECT_NEAR(724379, association, 1);
-    EXPECT_NEAR(44799.20, connectivity, 1);
-    EXPECT_NEAR(166810, sparsity, 1);
+    EXPECT_NEAR(as.expected_association, association, 1);
+    EXPECT_NEAR(as.expected_connectivity, connectivity, 1);
+    EXPECT_NEAR(as.expected_sparsity, sparsity, 1);
 
 }
 
-TEST_P(SearchMarkers, check_lambda_eta) {
+TEST_P(SearchMarkers, checkLambdaAndEta) {
     auto as = GetParam();
 
     scones -> test_associations();
@@ -167,26 +111,76 @@ TEST_P(SearchMarkers, check_lambda_eta) {
     EXPECT_NEAR(as.expected_eta, eta, 1);
 }
 
-TEST_P(SearchMarkers, checkMarkers) {
+TEST_P(SearchMarkers, checkSelectedSNPS) {
+    auto as = GetParam();
+
     scones -> test_associations();
     int out = scones -> getIndicatorVector().sum();
     EXPECT_EQ(10, out);
-    EXPECT_EQ(1, scones -> getIndicatorVector()(676));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(679));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(680));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(682));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(684));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(685));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(686));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(690));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(695));
-    EXPECT_EQ(1, scones -> getIndicatorVector()(696));
+    for (unsigned int i = 0; i < sizeof(as.expected_causal_SNPs)/sizeof(as.expected_causal_SNPs[0]); i++){
+        EXPECT_EQ(1, scones -> getIndicatorVector()(as.expected_causal_SNPs[i]));
+    }
 }
+
+TEST_P(SearchMarkers, checkOutputFiles) {
+    auto as = GetParam();
+
+    scones -> test_associations();
+    VectorXd terms = scones -> getObjectiveFunctionTerms(lambda,eta);
+    VectorXd skat = scones -> getScoreStatistic();
+
+    string output_str = as.path_prefix + "/" + tmpData.phenotype_names[0] + ".scones.out.ext.txt";
+    CSconesIO::writeOutput(output_str, tmpData, scones -> getIndicatorVector(), scones -> getBestLambda(), scones -> getBestEta(), terms, skat);
+
+    output_str = as.path_prefix  + "/" + tmpData.phenotype_names[0] + ".scones.pmatrix.txt";
+    CSconesIO::writeCMatrix(output_str, scones -> getCMatrix(), scones -> getSettings());
+}
+
+CSconesInitialSettings gridSearchParams_skat = CSconesInitialSettings {
+        -1, // eta
+        16681, // expected_eta
+        -1, // lambda
+        278.25, // expected_lambda
+        SKAT, // test_statistic
+        724379, // expected_association
+        44799.20, // expected_connectivity
+        166810, // expected_sparsity
+        "data/testing/scones/skat/", // path_prefix
+        {676, 679, 680, 682, 684, 685, 686, 690, 695, 696} // expected_causal_SNPs
+};
+
+CSconesInitialSettings fixedParams_skat = CSconesInitialSettings {
+        17000, // eta
+        17000, // expected_eta
+        300, // lambda
+        300, // expected_lambda
+        SKAT, // test_statistic
+        724379, // expected_association
+        48300, // expected_connectivity
+        170000, // expected_sparsity
+        "data/testing/scones/skat/", // path_prefix
+        {676, 679, 680, 682, 684, 685, 686, 690, 695, 696} // expected_causal_SNPs
+};
+
+CSconesInitialSettings gridSearchParams_chisq = CSconesInitialSettings {
+        41.25, // eta
+        16681, // expected_eta
+        8.38e-12, // lambda
+        278.25, // expected_lambda
+        CHISQ, // test_statistic
+        724379, // expected_association
+        44799.20, // expected_connectivity
+        166810, // expected_sparsity
+        "data/testing/scones/skat/", // path_prefix
+        {676, 679, 680, 682, 684, 685, 686, 690, 695, 696} // expected_causal_SNPs
+
+};
 
 INSTANTIATE_TEST_CASE_P(checkParameters, SearchMarkers,
     testing::Values(
-            CSconesInitialSettings{-1, 278.25, -1, 16681},
-            CSconesInitialSettings{300, 300, 17000, 17000}
+            gridSearchParams_skat,
+            fixedParams_skat,
+            gridSearchParams_chisq
     ));
 
 
